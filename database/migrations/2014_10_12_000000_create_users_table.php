000ac1179c0fa43a817e9a7705c47d69e02c08ef
usbbush <jeanbienvenusb@gmail.com>
2023-01-10 22:11:53 +0200

    Create user migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,10 +13,24 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
+        Schema::create('types', function (Blueprint $table) {
+            $table->bigIncrements('id');
             $table->string('name');
-            $table->string('email')->unique();
+            $table->timestamps();
+        });
+        Schema::create('users', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name',100);
+            $table->boolean('blocked');
+            $table->bigInteger('type_id')->unsigned();
+            $table->foreign('type_id')->references('id')->on('types')->onDelete('cascade');
+            $table->string('country',150);
+            $table->string('image');
+            $table->string('ministry_church',150);
+            $table->string('ministry_type');
+            $table->string('phone');
+            $table->boolean('contact_visibility');
+            $table->string('email',100)->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
