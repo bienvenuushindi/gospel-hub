05a041abc2c5807b6ae648c13f5502c0efb90b6e
usbbush <jeanbienvenusb@gmail.com>
2023-01-17 13:27:20 +0200

    Implement EmailSubscription model and associations

---
+++ app/Models/EmailSubscription.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class EmailSubscription extends Model
+{
+    //
+//    protected $fillable = [
+//        'email',
+//        'name',
+//        'subscribed',
+//    ];
+    protected $guarded = [];
+}
